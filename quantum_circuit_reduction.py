6f45165a7f593c109b0b101fe63e60edf2df2f7c
Renata Wong <renata.wong.kei@gmail.com>
2023-08-10 14:36:11 +0800

    Fixed issues with lower wires not being reduced

--- quantum_circuit_reduction.py
+++ quantum_circuit_reduction.py
@@ -14,73 +14,71 @@ class GateReduction(TransformationPass):
     
     """
     A transpiler pass to reduce gate number in a circuit.
+    Rules: XX=YY=ZZ=HH=I, SdgS=SSdg=I, CXCX=II
     """
 
     def run(self, dag):
 
-        # iterate over all operations in a circuit
-        nodes = dag.op_nodes()
-        num_nodes = len(nodes)
-        
-        for index, node in enumerate(nodes):
+        # iterate over all operations, wire after wire
+        for wire in dag.wires:
             
-            if node.op.name in ['x', 'y', 'z', 'h', 'cx']:
-                
-                if index+1 < num_nodes:
-                    next_node = nodes[index+1]
-                    if next_node.op.name == node.op.name:
-                        if node.op.name == 'cx':
+            # convert iterator nodes_on_wire to a list of nodes
+            nodes = list(dag.nodes_on_wire(wire, only_ops = True)) 
+            
+            for index, node in enumerate(nodes): 
+
+                if node.op.name in ['x', 'y', 'z', 'h', 'cx']:
+                    
+                    # warning: dag.node_counter produces incorrect values for our use
+                    if index+1 < len(nodes): 
+                        next_node = nodes[index+1]
+                        if next_node.op.name == node.op.name:
                             if node.qargs == next_node.qargs:
-                                replacement = QuantumCircuit(2)
-                                dag.substitute_node_with_dag(node, circuit_to_dag(replacement))
+                                dag.remove_op_node(node)
+                                dag.remove_op_node(next_node)
+
+                        if index+2 < len(nodes):
+                            if node.op.name == 'h' and next_node.op.name == 'z':
+                                third_node = nodes[index+2]
+                                if third_node.op.name == 'h':
+                                    replacement = QuantumCircuit(1)
+                                    replacement.x(0)
+                                    dag.substitute_node_with_dag(third_node, circuit_to_dag(replacement))
+                                    dag.remove_op_node(node)
+                                    dag.remove_op_node(next_node)
+
+                        if index+2 < len(nodes):
+                            if node.op.name == 'h' and next_node.op.name == 'x':
+                                third_node = nodes[index+2]
+                                if third_node.op.name == 'h':
+                                    replacement = QuantumCircuit(1)
+                                    replacement.z(0)
+                                    dag.substitute_node_with_dag(third_node, circuit_to_dag(replacement))
+                                    dag.remove_op_node(node)
+                                    dag.remove_op_node(next_node)
+
+
+                if node.op.name in ['s', 'sdg']:
+
+                    if index+1 < len(nodes): 
+                        next_node = nodes[index+1]
+                        if next_node.op.name in ['s', 'sdg']:
+                            if next_node.op.name != node.op.name:
+                                dag.remove_op_node(node)
+                                dag.remove_op_node(next_node)
+
+                            elif next_node.op.name == node.op.name:
+                                dag.remove_op_node(node)
+                                replacement = QuantumCircuit(1)
+                                replacement.z(0)
                                 dag.substitute_node_with_dag(next_node, circuit_to_dag(replacement))
 
-                        else:
-                            replacement = QuantumCircuit(1)
-                            dag.substitute_node_with_dag(node, circuit_to_dag(replacement))
-                            dag.substitute_node_with_dag(next_node, circuit_to_dag(replacement))
-                        
-                    if node.op.name == 'h' and next_node.op.name == 'z':
-                        third_node = nodes[index+2]
-                        if third_node.op.name == 'h':
-                            replacement = QuantumCircuit(1)
-                            dag.substitute_node_with_dag(node, circuit_to_dag(replacement))
-                            dag.substitute_node_with_dag(next_node, circuit_to_dag(replacement))
-                            replacement.x(0)
-                            dag.substitute_node_with_dag(third_node, circuit_to_dag(replacement))
-                        
-                    if node.op.name == 'h' and next_node.op.name == 'x':
-                        third_node = nodes[index+2]
-                        if third_node.op.name == 'h':
-                            replacement = QuantumCircuit(1)
-                            dag.substitute_node_with_dag(node, circuit_to_dag(replacement))
-                            dag.substitute_node_with_dag(next_node, circuit_to_dag(replacement))
-                            replacement.z(0)
-                            dag.substitute_node_with_dag(third_node, circuit_to_dag(replacement))
-
-                        
-            if node.op.name in ['s', 'sdg']:
-                
-                if index+1 < num_nodes:
-                    next_node = nodes[index+1]
-                    if next_node.op.name in ['s', 'sdg'] and next_node.op.name != node.op.name:
-                        replacement = QuantumCircuit(1)
-                        # replace the node with our new decomposition
-                        dag.substitute_node_with_dag(node, circuit_to_dag(replacement))
-                        dag.substitute_node_with_dag(next_node, circuit_to_dag(replacement))
-                        
-                    if next_node.op.name in ['s', 'sdg'] and next_node.op.name == node.op.name:
-                        replacement = QuantumCircuit(1)
-                        replacement.z(0)
-                        # replace the node with our new decomposition
-                        dag.substitute_node_with_dag(node, circuit_to_dag(replacement))
-                        dag.substitute_node_with_dag(next_node, circuit_to_dag(replacement))
-        
         return dag
 
-    
-    
-    
+
+
+
+
 def reduce_circuit_depth(circuit: QuantumCircuit):
     
     """
@@ -93,17 +91,14 @@ def reduce_circuit_depth(circuit: QuantumCircuit):
         reduced_circuit:
             the reduced circuit
     """
-    
-    qc_gate_number = circuit.depth()
+    circuit_size = circuit.size()
+    reduced_circuit = circuit.copy()
     
     while True:
-        reduced_circuit = GateReduction()(circuit)
-        reduced_gate_number = reduced_circuit.depth()
-
-        if qc_gate_number == reduced_gate_number:
+        reduced_circuit = GateReduction()(reduced_circuit) 
+        if circuit_size == reduced_circuit.size():
             break
         else:
-            qc_gate_number = reduced_gate_number
-        
+            circuit_size = reduced_circuit.size()
+
     return reduced_circuit
-        
